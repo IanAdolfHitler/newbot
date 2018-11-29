3c8b78a96028e2fed84710644966fd41c130230e
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-11-29 22:53:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -68,6 +68,10 @@ def Reply(text):
         return "再吵我槍斃你"
     elif text=="你有什麼願望呢":
         return "客家人同性戀三餐都吃核廢料 通通關進毒氣室"
+    elif text=="意欣":
+        return "欠扁的小女孩!?"
+    elif text=="范意欣":
+        return "別再熬夜拉"
 
 
 import os
