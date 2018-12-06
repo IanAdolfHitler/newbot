a98a821e3e953f6ef1f08fb22db63c3940974908
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-06 18:56:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -38,6 +38,32 @@ def KeyWord(text):
 		if text.find(k) !=-1:
 			return [True,key[k]]
 	return [False]
+def Button():
+	message = TemplateSendMessage(
+    alt_text='Buttons template',
+    template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='Menu',
+        text='Please select',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            ),
+            URITemplateAction(
+                label='uri',
+                uri='http://example.com/'
+            )
+        ]
+    )
+)
+line_bot_api.reply_message(event.reply_token, message)
+
 def Reply(event):
 	ktemp=KeyWord(event.message.text)
 	if ktemp[0]:
