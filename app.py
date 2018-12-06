fa4b799546d55611bf33c56418b88ad2aa1b1ffb
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-06 19:15:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -41,17 +41,17 @@ def KeyWord(text):
 def Button(event):
     message = TemplateSendMessage(
         alt_text='Buttons template',
-   	    template=ButtonsTemplate(
+        template=ButtonsTemplate(
             thumbnail_image_url='https://example.com/image.jpg',
-       	    title='Menu',
-       	    text='Please select',
+            title='Menu',
+            text='Please select',
             actions=[
                 PostbackTemplateAction(
                     label='postback',
                     text='postback text',
                     data='action=buy&itemid=1'
                 ),
-           	    MessageTemplateAction(
+                MessageTemplateAction(
                     label='message',
                     text='message text'
                 ),
