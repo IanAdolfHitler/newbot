38712d3c76575f390099c21f301c8a93936407a4
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-07 21:14:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2 + J9 + GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89 / 1O / w1cDnyilFU =')
+line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2+J9+GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('0963231f909332b42026ae9515df6757')
 
@@ -29,58 +29,48 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 def KeyWord(text):
-    KeyWordDict = {"你好":"你也好啊",
-                   "你是誰":"我是大帥哥",
-                   "帥":"帥炸了"}
+    KeyWordDict = {"嗨":"你這死賤畜",
+                   "打":"你別再戳了好不好 去洗澡",
+                   "豪":"說謊的人要吞一千斤精喔"
+                   "來":"來什麼來 我就是要去妳妹的"
+                   "玉米":"你這個母胎單身的魯蛇 老二比茄子還臭"
+                   "嗨哥":"嗨哥在吞糞 別吵牠"
+                   "肚臍":"守護家園 有你沒有我"
+                   "加林":"都交女朋友了 什麼時候讓我們交流一下啊"
+                   "育朕":"你的彩蛋太難想了 從缺"
+                   "db":"別再找了 就是沒有你的"
+                   "底逼":"放棄吧  你是被遺忘的"
+                   "邢":"噓 別說那個人的名字 他會吃定你的蛋蛋的.."
+                   "晚點":"每次說晚點都等到下禮拜了 你怎不晚點再尻槍"
+                   "等等":"要多久 等你老婆懷上我的孩子嗎"
+                   "==":"再吵我強姦你 讓你從嘴巴到屁眼都是我的洨"
+                   ".":"說不出話來就去喝尿阿"
+                   "哲":"嗨哥 我們也都是哲漢喔"
+                   "芷":"宥軒 你是你鬥劍是鬥不過哲漢的 回去把綠帽戴好"
+                   "累":"累啥 我把你操到中華民國萬萬歲喔"
+                   "分鐘":"喔 是喔"
+                   "有人":"這裡沒你的事 滾"
+                   "滾":"好 我走 但是在我把你弄到懷孕後ㄎㄎ"
+                   "快":"等你弄完換 我把你操的兵兵乓乓"}
     for k in KeyWordDict.keys():
         if text.find(k) != -1:
             return [True,KeyWordDict[k]]
     return [False]
 
-def Button(event):
-    message = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://github.com/IanAdolfHitler/newbot/blob/master/1.jpg?raw=true',
-            title='Menu',
-            text='Please select',
-            actions=[
-                PostbackTemplateAction(
-                    label='postback',
-                    text='postback text',
-                    data='action=buy&itemid=1'
-                ),
-                MessageTemplateAction(
-                    label='message',
-                    text='message text'
-                ),
-                URITemplateAction(
-                    label='uri',
-                    uri='http://example.com/'
-                )
-            ]
-        )
-    )
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text = Reply(event.message.text))
     line_bot_api.reply_message(event.reply_token, message)
-
 def Reply(event):
     Ktemp = KeyWord(event.message.text)
     if Ktemp[0]:
         line_bot_api.reply_message(event.reply_token,
             TextSendMessage(text = Ktemp[1]))
     else:
-        Button(event)
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    try:
-        Reply(event)
-    except Exception as e:
-        line_bot_api.reply_message(event.reply_token, 
-            TextSendMessage(text=str(e)))
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text = event.message.text))
 
 import os
 if __name__ == "__main__":
