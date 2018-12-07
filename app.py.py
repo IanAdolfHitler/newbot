d1820d067456d068e2cc21112fecd26d8c098e63
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-07 21:27:53 +0800

    Add files via upload

--- app.py.py
+++ app.py.py
@@ -11,7 +11,7 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2 + J9 + GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89 / 1O / w1cDnyilFU =')
+line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2+J9+GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('0963231f909332b42026ae9515df6757')
 
@@ -29,7 +29,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 def KeyWord(text):
     KeyWordDict = {"嗨":"你這死賤畜",
                    "打":"你別再戳了好不好 去洗澡",
@@ -59,22 +58,19 @@ def KeyWord(text):
             return [True,KeyWordDict[k]]
     return [False]
 
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text = Reply(event.message.text))
+    line_bot_api.reply_message(event.reply_token, message)
 def Reply(event):
     Ktemp = KeyWord(event.message.text)
     if Ktemp[0]:
         line_bot_api.reply_message(event.reply_token,
             TextSendMessage(text = Ktemp[1]))
     else:
-        Button(event)
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    try:
-        Reply(event)
-    except Exception as e:
-        line_bot_api.reply_message(event.reply_token, 
-            TextSendMessage(text=str(e)))
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text = event.message.text))
 
 import os
 if __name__ == "__main__":
