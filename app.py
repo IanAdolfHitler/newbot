6fe0119b681344dd1d3e3cc85b309c59283cb167
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-13 17:54:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,38 @@
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import *
 
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2+J9+GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('1WUu6duAA0P7ofV4J3earPdrX8masnaN6hdy6cTctFZJxTG1eWrE51wfbM6wBZtxFRU6qR2+J9+GGAxlIfWl5ZFi6blqDaLV6Pedy3YI8RDpMQjyhlkzQI1pDWwwsOgXGNSbLmpS4qwM96rSNYBFtwdB04t89/1O/w1cDnyilFU=
+')
 # Channel Secret
 handler = WebhookHandler('0963231f909332b42026ae9515df6757')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-#----------從這裡開始複製----------
-
-#關鍵字系統
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
+	return 'OK'
+#關鍵字
 def KeyWord(event):
-    KeyWordDict = {"嗨":"你這死賤畜",
+	KeyWordDict = {"嗨":"你這死賤畜",
                    "打":"你別再戳了好不好 去洗澡",
                    "豪":"說謊的人要吞一千斤精喔",
                    "來":"來什麼來 我就是要去妳妹的",
@@ -57,59 +55,56 @@ def KeyWord(event):
                    "有人":"這裡沒你的事 滾",
                    "滾":"好 我走 但是在我把你弄到懷孕後ㄎㄎ",
                    "快":"等你弄完 換我把你操的兵兵乓乓"}
-
-    for k in KeyWordDict.keys():
-        if event.message.text.find(k) != -1:
-            return [True,KeyWordDict[k]]
-    return [False]
-
-#按鈕版面系統
+	for k in KeyWordDict.keys():
+		if event.message.text.find(k) != -1:
+			return [True, KeyWordDict[k]]
+	return [False]
+#按鈕版面
 def Button(event):
-    return TemplateSendMessage(
-        alt_text='特殊訊息，請進入手機查看',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://github.com/54bp6cl6/LineBotClass/blob/master/logo.jpg?raw=true',
-            title='HPClub - Line Bot 教學',
-            text='大家學會了ㄇ',
-            actions=[
-                PostbackTemplateAction(
-                    label='還沒',
-                    data='這裡留空就好，不要刪掉'
-                ),
-                MessageTemplateAction(
-                    label='差不多了',
-                    text='差不多了'
-                ),
-                URITemplateAction(
-                    label='幫我們按個讚',
-                    uri='https://www.facebook.com/ShuHPclub'
-                )
-            ]
-        )
-    )
-
-#回覆函式
+	return TemplateSendMessage(
+		alt_text='yeeee',
+		template=ButtonsTemplate(
+			thumbnail_image_url='https://github.com/leavingink/muyang/blob/master/sheep.png?raw=true',
+			title='Eternal',
+			text='呼叫',
+			actions=[
+				MessageTemplateAction(
+					label='',
+					text=''
+				),
+				MessageTemplateAction(
+					label='',
+					text=''
+				),
+				MessageTemplateAction(
+					label='',
+					text=''
+				)
+			]
+		)
+	)
+#回復函式
 def Reply(event):
-    Ktemp = KeyWord(event)
-    if Ktemp[0]:
-        line_bot_api.reply_message(event.reply_token,
-            TextSendMessage(text = Ktemp[1]))
-    else:
-        line_bot_api.reply_message(event.reply_token,
-            Button(event))
-
+	Ktemp = KeyWord(event)
+	if Ktemp[0]:
+		line_bot_api.reply_message(event.reply_token, 
+			TextSendMessage(text = Ktemp[1]))
+	elif event.message.text == "呼叫":
+		line_bot_api.reply_message(event.reply_token,
+			Button(event))
+	#else:
+		#line_bot_api.reply_message(event.reply_token,
+			#TextMessage(text = event.message.text))
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    try:
-        Reply(event)
-    except Exception as e:
-        line_bot_api.reply_message(event.reply_token, 
-            TextSendMessage(text=str(e)))
-
-#----------複製到這裡結束----------
-
+	try:
+		Reply(event)
+	except Exception as e:
+		line_bot_api.reply_message(event.reply_token,
+			TextSendMessage(text=str(e)))
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+
