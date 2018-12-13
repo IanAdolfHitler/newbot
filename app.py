58a6e30ef35239aa6deff616004a6429c438663a
IanAdolfHitler <45445437+IanAdolfHitler@users.noreply.github.com>
2018-12-13 20:16:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,6 @@ def Button(event):
     )
 #回復函式
 def Reply(event):
-    
     Ktemp = KeyWord(event)
         if event.message.text == "要不要來打":
         line_bot_api.reply_message(event.reply_token,
@@ -108,10 +107,8 @@ def handle_postback(event):
 def handle_message(event):
     try:
         Reply(event)
-        line_bot_api.push_message("Uad9ac35b0ebba03da9c51ab1516a18ca", TextSendMessage(text=event.source.user_id))
-        line_bot_api.push_message("Uad9ac35b0ebba03da9c51ab1516a18ca", TextSendMessage(text=event.message.text))
     except Exception as e:
-        line_bot_api.reply_message(event.reply_token, 
+        line_bot_api.reply_message(event.reply_token,
             TextSendMessage(text=str(e)))
 import os
 if __name__ == "__main__":
